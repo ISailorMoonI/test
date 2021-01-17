6a361243ce9a7b104e626f14e2fcba474736245c
SailorMoon <yashamaru99@gmail.com>
2021-01-17 13:16:15 +0300

    added files asteroid

---
+++ AsteroidGame/AsteroidGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsteroidGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
